f8e87678603face271d9f6b306d5fbeb79e3d4e8
Maksim Sheko <sakusan@mail.ru>
2024-01-16 23:40:31 +0300

    hot fix

--- js/app.js
+++ js/app.js
@@ -45,4 +45,87 @@ const categorySlider = new Swiper('.category__swiper', {
     }
 });
 
+const newsSlider = new Swiper('.news__swiper', {
+    spaceBetween: 0,
+    slidesPerView: 5,
+    navigation: {
+        nextEl: '.news__button.next',
+        prevEl: '.news__button.prev',
+    },
+    breakpoints: {
+        0: {
+            spaceBetween: 10,
+            slidesPerView: 'auto',
+        },
+        798: {
+            slidesPerView: 3,
+        },
+        1022: {
+            slidesPerView: 4,
+        },
+        1230: {
+            slidesPerView: 5,
+        },
+    }
+});
+
+const brandsSwiper = new Swiper('.brands__swiper', {
+    navigation: {
+        nextEl: '.brands__button.next',
+        prevEl: '.brands__button.prev',
+    },
+    breakpoints: {
+        0: {
+            spaceBetween: 10,
+            slidesPerView: 'auto',
+        },
+        798: {
+            spaceBetween: 10,
+            slidesPerView: 2,
+        },
+        1022: {
+            spaceBetween: 18, 
+            slidesPerView: 3,
+        },
+        1230: {
+            spaceBetween: 18, 
+            slidesPerView: 4,
+        },
+    }
+});
+
 //End sliders
+
+//Start Card products
+
+const cardProductsBtn = document.querySelectorAll('[data-button="card-product"]');
+const cardProductsCounters = document.querySelectorAll('.product-card__counter');
+cardProductsBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        btn.style.display = 'none';
+        cardProductsCounters[index].classList.add('active');
+    })
+})
+
+//End Card products
+
+//Start counters
+
+const buttonsPlus =  document.querySelectorAll('[data-button="plus"]');
+const buttonsMinus =  document.querySelectorAll('[data-button="minus"]');
+const countDisplays =  document.querySelectorAll('[data-block="count"]');
+
+buttonsPlus.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        countDisplays[index].textContent = Number(countDisplays[index].textContent) + 1;
+    })
+})
+buttonsMinus.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        if (Number(countDisplays[index].textContent) !== 1) {
+            countDisplays[index].textContent = Number(countDisplays[index].textContent) - 1;
+        }
+    })
+})
+
+//End counters
